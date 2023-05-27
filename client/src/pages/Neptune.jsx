3d6ff496ce358ea1ca140f69fbb75315b561f183
Dylan Mac Yves <macyvesdylan@gmail.com>
2023-05-27 21:18:13 +0700

    neptune

--- client/src/pages/Neptune.jsx
+++ client/src/pages/Neptune.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import NeptuneImage from "../assets/images/neptune.png";
+import NeptuneMoon from "../assets/images/neptune-moon.png";
+import NeptuneShip from "../assets/images/space-ship.png";
 
 function Neptune(props) {
     return (
@@ -32,7 +34,7 @@ function Neptune(props) {
                 </div>
                 <div
                     data-aos="fade-up"
-                    className="min-h-[200vh] text-white mt-20 max-w-xl m-auto"
+                    className="min-h-[50vh] text-white mt-20 max-w-xl m-auto"
                 >
                     <h3 className="text-2xl mb-4">Did you know?</h3>
                     <ul className="space-y-4">
@@ -50,7 +52,147 @@ function Neptune(props) {
                         </li>
                     </ul>
                 </div>
-                <h1>HEllo</h1>
+                <div className="min-h-screen-[200vh]">
+                    <div
+                        id="default-carousel"
+                        class="relative w-full"
+                        data-carousel="slide"
+                    >
+                        <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+                            <div
+                                class="hidden duration-700 ease-in-out relative"
+                                data-carousel-item
+                            >
+                                <img
+                                    className="absolute left-1/2 -translate-x-1/2"
+                                    src={NeptuneMoon}
+                                    alt=""
+                                />
+                            </div>
+                            <div
+                                class="hidden duration-700 ease-in-out relative"
+                                data-carousel-item
+                            >
+                                <img src={NeptuneShip} alt="" />
+                            </div>
+                            <div
+                                class="hidden duration-700 ease-in-out relative"
+                                data-carousel-item
+                            >
+                                <img
+                                    className="absolute left-1/2 -translate-x-1/2"
+                                    src={NeptuneImage}
+                                    alt=""
+                                />
+                            </div>
+                            <div
+                                class="hidden duration-700 ease-in-out relative"
+                                data-carousel-item
+                            >
+                                <img
+                                    className="absolute left-1/2 -translate-x-1/2"
+                                    src={NeptuneImage}
+                                    alt=""
+                                />
+                            </div>
+                            <div
+                                class="hidden duration-700 ease-in-out relative"
+                                data-carousel-item
+                            >
+                                <img
+                                    className="absolute left-1/2 -translate-x-1/2"
+                                    src={NeptuneImage}
+                                    alt=""
+                                />
+                            </div>
+                        </div>
+                        <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+                            <button
+                                type="button"
+                                class="w-3 h-3 rounded-full"
+                                aria-current="true"
+                                aria-label="Slide 1"
+                                data-carousel-slide-to="0"
+                            ></button>
+                            <button
+                                type="button"
+                                class="w-3 h-3 rounded-full"
+                                aria-current="false"
+                                aria-label="Slide 2"
+                                data-carousel-slide-to="1"
+                            ></button>
+                            <button
+                                type="button"
+                                class="w-3 h-3 rounded-full"
+                                aria-current="false"
+                                aria-label="Slide 3"
+                                data-carousel-slide-to="2"
+                            ></button>
+                            <button
+                                type="button"
+                                class="w-3 h-3 rounded-full"
+                                aria-current="false"
+                                aria-label="Slide 4"
+                                data-carousel-slide-to="3"
+                            ></button>
+                            <button
+                                type="button"
+                                class="w-3 h-3 rounded-full"
+                                aria-current="false"
+                                aria-label="Slide 5"
+                                data-carousel-slide-to="4"
+                            ></button>
+                        </div>
+                        <button
+                            type="button"
+                            class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                            data-carousel-prev
+                        >
+                            <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                                <svg
+                                    aria-hidden="true"
+                                    class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M15 19l-7-7 7-7"
+                                    ></path>
+                                </svg>
+                                <span class="sr-only">Previous</span>
+                            </span>
+                        </button>
+                        <button
+                            type="button"
+                            class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                            data-carousel-next
+                        >
+                            <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                                <svg
+                                    aria-hidden="true"
+                                    class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M9 5l7 7-7 7"
+                                    ></path>
+                                </svg>
+                                <span class="sr-only">Next</span>
+                            </span>
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
