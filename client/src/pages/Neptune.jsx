b4c38276cfdb56660400f49af8ad4202b81b31d4
Dylan Mac Yves <macyvesdylan@gmail.com>
2023-05-27 22:11:02 +0700

    modified neptune

--- client/src/pages/Neptune.jsx
+++ client/src/pages/Neptune.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import NeptuneImage from "../assets/images/neptune.png";
 import NeptuneMoon from "../assets/images/neptune-moon.png";
 import NeptuneShip from "../assets/images/space-ship.png";
@@ -19,7 +20,7 @@ function Neptune(props) {
                             NEPTUNE
                         </h1>
                     </div>
-                    <p className="text-white max-w-xl text-center m-auto mt-10">
+                    <p className="text-white max-w-xl text-center m-auto mt-10 font-light">
                         <span className="font-semibold text-[#DBCC49] mr-1">
                             Neptune
                         </span>
@@ -32,165 +33,74 @@ function Neptune(props) {
                         world.
                     </p>
                 </div>
+
                 <div
                     data-aos="fade-up"
                     className="min-h-[50vh] text-white mt-20 max-w-xl m-auto"
                 >
-                    <h3 className="text-2xl mb-4">Did you know?</h3>
-                    <ul className="space-y-4">
-                        <li className="bg-[#BFDBF7] bg-opacity-25 p-2 rounded-sm">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Omnis?
+                    <h3 className="text-2xl mb-10">Did you know?</h3>
+                    <ul className="space-y-8">
+                        <li
+                            data-aos="fade-right"
+                            data-aos-duration="1500"
+                            className="bg-[#BFDBF7] border-l-8 border-[#DBCC49] bg-opacity-25 p-3 font-light rounded-sm relative md:-left-10"
+                        >
+                            It has a total of 14 known moons, with Triton being
+                            the largest and most significant. Triton is unique
+                            among large moons in our solar system because it
+                            orbits in the opposite direction of Neptune's
+                            rotation
                         </li>
-                        <li className="bg-[#BFDBF7] bg-opacity-25 p-2 rounded-sm">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Omnis?
+                        <li
+                            data-aos="fade-left"
+                            data-aos-duration="1500"
+                            className="bg-[#BFDBF7] border-l-8 border-[#DBCC49] bg-opacity-25 p-3 font-light rounded-sm relative md:left-10"
+                        >
+                            The blue coloring is the result of methane in the
+                            atmosphere, though the exact reason for the
+                            vividness of the blue is still unknown.
                         </li>
-                        <li className="bg-[#BFDBF7] bg-opacity-25 p-2 rounded-sm">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Omnis?
+                        <li
+                            data-aos="fade-right"
+                            data-aos-duration="1500"
+                            className="bg-[#BFDBF7] border-l-8 border-[#DBCC49] bg-opacity-25 p-3 font-light rounded-sm relative md:-left-10"
+                        >
+                            Neptune was named after the Roman god of the sea due
+                            to its blue appearance, reminiscent of the deep
+                            ocean.
                         </li>
                     </ul>
                 </div>
-                <div className="min-h-screen-[200vh]">
-                    <div
-                        id="default-carousel"
-                        class="relative w-full"
-                        data-carousel="slide"
-                    >
-                        <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-                            <div
-                                class="hidden duration-700 ease-in-out relative"
-                                data-carousel-item
-                            >
-                                <img
-                                    className="absolute left-1/2 -translate-x-1/2"
-                                    src={NeptuneMoon}
-                                    alt=""
-                                />
-                            </div>
-                            <div
-                                class="hidden duration-700 ease-in-out relative"
-                                data-carousel-item
-                            >
-                                <img src={NeptuneShip} alt="" />
-                            </div>
-                            <div
-                                class="hidden duration-700 ease-in-out relative"
-                                data-carousel-item
-                            >
-                                <img
-                                    className="absolute left-1/2 -translate-x-1/2"
-                                    src={NeptuneImage}
-                                    alt=""
-                                />
-                            </div>
-                            <div
-                                class="hidden duration-700 ease-in-out relative"
-                                data-carousel-item
-                            >
-                                <img
-                                    className="absolute left-1/2 -translate-x-1/2"
-                                    src={NeptuneImage}
-                                    alt=""
-                                />
-                            </div>
-                            <div
-                                class="hidden duration-700 ease-in-out relative"
-                                data-carousel-item
-                            >
-                                <img
-                                    className="absolute left-1/2 -translate-x-1/2"
-                                    src={NeptuneImage}
-                                    alt=""
-                                />
-                            </div>
-                        </div>
-                        <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-                            <button
-                                type="button"
-                                class="w-3 h-3 rounded-full"
-                                aria-current="true"
-                                aria-label="Slide 1"
-                                data-carousel-slide-to="0"
-                            ></button>
-                            <button
-                                type="button"
-                                class="w-3 h-3 rounded-full"
-                                aria-current="false"
-                                aria-label="Slide 2"
-                                data-carousel-slide-to="1"
-                            ></button>
-                            <button
-                                type="button"
-                                class="w-3 h-3 rounded-full"
-                                aria-current="false"
-                                aria-label="Slide 3"
-                                data-carousel-slide-to="2"
-                            ></button>
-                            <button
-                                type="button"
-                                class="w-3 h-3 rounded-full"
-                                aria-current="false"
-                                aria-label="Slide 4"
-                                data-carousel-slide-to="3"
-                            ></button>
-                            <button
-                                type="button"
-                                class="w-3 h-3 rounded-full"
-                                aria-current="false"
-                                aria-label="Slide 5"
-                                data-carousel-slide-to="4"
-                            ></button>
-                        </div>
-                        <button
-                            type="button"
-                            class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-                            data-carousel-prev
+
+                <div
+                    data-aos="fade-up"
+                    data-aos-duration="1500"
+                    className="min-h-[125vh] max-w-xl m-auto mt-48"
+                >
+                    <h3 className="text-2xl mb-4 text-white">
+                        Future discoveries
+                    </h3>
+                    <div>
+                        <a
+                            href="#"
+                            class=" flex flex-col items-center bg-opacity-80 bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
                         >
-                            <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                                <svg
-                                    aria-hidden="true"
-                                    class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M15 19l-7-7 7-7"
-                                    ></path>
-                                </svg>
-                                <span class="sr-only">Previous</span>
-                            </span>
-                        </button>
-                        <button
-                            type="button"
-                            class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-                            data-carousel-next
-                        >
-                            <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                                <svg
-                                    aria-hidden="true"
-                                    class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M9 5l7 7-7 7"
-                                    ></path>
-                                </svg>
-                                <span class="sr-only">Next</span>
-                            </span>
-                        </button>
+                            <img
+                                class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+                                src="/docs/images/blog/image-4.jpg"
+                                alt=""
+                            />
+                            <div class="flex flex-col justify-between p-4 leading-normal">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                    Noteworthy technology acquisitions 2021
+                                </h5>
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                    Here are the biggest enterprise technology
+                                    acquisitions of 2021 so far, in reverse
+                                    chronological order.
+                                </p>
+                            </div>
+                        </a>
                     </div>
                 </div>
             </div>
